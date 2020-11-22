a8d267afa82b07d9cba7d60c784fd2abca62282c
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-11-22 15:00:25 +0700

    ok

--- build_fire_fox.sh
+++ build_fire_fox.sh
@@ -3,5 +3,5 @@ tar xf ./phoronix-test-suite-10.0.1.tar.gz
 ls -lha
 cd phoronix-test-suite
 ls -lha
-printf '%s\n%s\n%s\n' y y 3 | ./phoronix-test-suite benchmark blender
+printf '%s\n%s\n%s\n' y y 3 | ./phoronix-test-suite benchmark compress-7zip
 # yes | ./phoronix-test-suite run pts/x265
