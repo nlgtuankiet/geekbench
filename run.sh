555aa3282d5abd016c4c6c3e32b0ed88fe88293f
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-11-22 14:32:36 +0700

    ok

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 curl https://cdn.geekbench.com/Geekbench-5.3.1-Linux.tar.gz -o ./Geekbench-5.3.1-Linux.tar.gz
 tar xf ./Geekbench-5.3.1-Linux.tar.gz
 ls -lha
-./Geekbench-5.3.1-Linux/geekbench5 --cpu
+./Geekbench-5.3.1-Linux/geekbench_x86_64 --cpu
