44f97374afab4850e90dbab3b68dda299f4153f9
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-11-22 13:49:06 +0700

    ok

--- build_fire_fox.sh
+++ build_fire_fox.sh
@@ -3,5 +3,5 @@ tar xf ./phoronix-test-suite-10.0.1.tar.gz
 ls -lha
 cd phoronix-test-suite
 ls -lha
-./phoronix-test-suite install pts/build-ffmpeg
+printf '%s\n%s\n' y y | ./phoronix-test-suite install pts/build-ffmpeg
 printf '%s\n%s\n%s\n\n\n' y r r | ./phoronix-test-suite run pts/build-ffmpeg
