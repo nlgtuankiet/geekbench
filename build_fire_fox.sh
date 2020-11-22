1c28753929479708456b69aa15900cc2118e880d
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-11-22 15:39:55 +0700

    ok

--- build_fire_fox.sh
+++ build_fire_fox.sh
@@ -6,5 +6,7 @@ cd phoronix-test-suite
 ls -lha
 mkdir ~/.phoronix-test-suite
 cp ./p_config.xml ~/.phoronix-test-suite/user-config.xml
-printf '%s\n%s\n' y y | ./phoronix-test-suite batch-benchmark compress-7zip
+printf '%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n%s\n' y y Y N Y n n n Y | ./phoronix-test-suite batch-setup
+# printf '%s\n%s\n' y y | 
+./phoronix-test-suite batch-benchmark compress-7zip
 # yes | ./phoronix-test-suite run pts/x265
