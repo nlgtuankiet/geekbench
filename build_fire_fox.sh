f98c606929e1d2e72ac20b03c7caa5d97bd05a25
nlgtuankiet <nlg.tuan.kiet@gmail.com>
2020-11-22 13:29:47 +0700

    ok

--- build_fire_fox.sh
+++ build_fire_fox.sh
@@ -1,4 +1,4 @@
 curl https://phoronix-test-suite.com/releases/phoronix-test-suite-10.0.1.tar.gz -o ./phoronix-test-suite-10.0.1.tar.gz
 tar xf ./phoronix-test-suite-10.0.1.tar.gz
 ls -lha
-./phoronix-test-suite-10.0.1/phoronix-test-suite install pts/build-firefox
+./phoronix-test-suite/phoronix-test-suite install pts/build-firefox
